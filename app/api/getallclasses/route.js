ed66a6e1de5af5f661ad1f9ef88b2e8ffa87c34c
Sahil <sanvekar108@gmail.com>
2024-08-11 23:54:28 +0530

    push 6

--- app/api/getallclasses/route.js
+++ app/api/getallclasses/route.js
@@ -1,36 +1,14 @@
 import connectToMongo from "@/middleware/connectToMongo";
 import Class from "@/models/Class";
 
-const allowedOrigins = ['https://heliverse-assignment-one.vercel.app'];
-
 const handler = async (req) => {
     try {
 
-        // CORS handling
-        const origin = req.headers.origin;
-        if (allowedOrigins.includes(origin)) {
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        }
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-        // Handle preflight OPTIONS request
-        if (req.method === 'OPTIONS') {
-            return res.status(200).end();
-        }
-
         const classes = await Class.find({});
 
-        if(classes){
-            return new Response(JSON.stringify({ classes }), {
-                status: 200
-            });
-        }
-        else{
-            return new Response(JSON.stringify({ classes : [] }), {
-                status: 200
-            });
-        }
+        return new Response(JSON.stringify({ classes }), {
+            status: 200
+        });
 
     } catch (error) {
         return new Response(JSON.stringify({ error: "Server error. Please try again." }), {
--- middleware/connectToMongo.js
+++ middleware/connectToMongo.js
@@ -5,21 +5,21 @@ const allowedOrigins = ['https://heliverse-assignment-one.vercel.app'];
 const connectToMongo = handler => async(req, res)=>{
 
     // CORS handling
-    const origin = req.headers.origin;
+    /*const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');*/
 
     // Handle preflight OPTIONS request
-    if (req.method === 'OPTIONS') {
+    /*if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
 
     if(mongoose.connections[0].readyState){
         return handler(req, res);
-    }
+    }*/
 
     await mongoose.connect(process.env.MONGO_URI);
     return handler(req, res);
